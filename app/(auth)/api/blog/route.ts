00af70a046924fceafa727ccdfc54c093fb86478
Ghuddy <mahmudtalal2@gmail.com>
2024-01-20 22:32:33 +0600

    add prisma

--- app/(auth)/api/blog/route.ts
+++ app/(auth)/api/blog/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
-  const getAllBlogs = await prisma.blog.findMany();
+  const getAllBlogs = await prisma?.blog?.findMany();
   console.log(getAllBlogs);
   return getAllBlogs;
 }
